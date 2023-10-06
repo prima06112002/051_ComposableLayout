b4d6263f9bdf3adf076666a838a259944b8eeda7
Prima06112002 <ramadhan06112002@gmail.com>
2023-10-06 16:16:49 +0700

    activity3composs

---
+++ app/src/main/java/com/example/komposclassb/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.example.komposclassb
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.komposclassb.ui.theme.KomposclassbTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KomposclassbTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+
+                ) {
+                    TampilLayar(pesan = getString(R.string.greeting),
+                        dari = getString(R.string.name) )
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun TampilText(message:String, from:String, modifier: Modifier=Modifier){
+    Column( verticalArrangement = Arrangement.Center, modifier =  modifier){
+        Text(
+            color = Color.Blue,
+            text = message,
+            fontSize = 80.sp,
+            lineHeight = 110.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            color = Color.Red,
+            text = from,
+            fontSize = 60.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+
+        )
+
+    }
+}
+@Composable
+fun TampilLayar(pesan: String, dari: String){
+    val image = painterResource(R.drawable.poto)
+    Box{
+        Image(painter = image, contentDescription = null,
+            contentScale = ContentScale.FillHeight,
+            modifier = Modifier.fillMaxSize())
+        TampilText(message = pesan, from = dari,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp))
+    }
+}
+
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    KomposclassbTheme {
+        TampilLayar(pesan = "Selamat Datang", dari ="Di UMY" )
+        Greeting("Android")
+    }
+}
